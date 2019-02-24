b1758148909b5102744bd7dd2bb75d80c12e9568
lawlor <shep34@msn.com>
2019-02-24 13:35:14 +0000

    img update

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -2,7 +2,7 @@ module.exports = [
   {
     username: 'tickle122',
     name: 'Tom Tickle',
-    avatar_url: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/4d5edb74-39fd-48ca-958a-fa9b127aa1f3/d7jq1tb-74876378-a75d-472f-97f1-fb1d9a83d5d3.jpg/v1/fill/w_762,h_1048,q_70,strp/the_terrifying_curse_of_mr_tickle___by_ticklegas_d7jq1tb-pre.jpg',
+    avatar_url: 'http://clipart-library.com/images/pc7rMReqi.jpg',
     // avatar_url: 'https://www.spiritsurfers.net/monastery/wp-content/uploads/_41500270_mrtickle.jpg',
   },
   {
@@ -32,7 +32,7 @@ module.exports = [
   {
     username: 'jessjelly',
     name: 'Jess Jelly',
-    avatar_url: 'https://www.abc.net.au/news/image/4955112-3x2-940x627.jpg',
+    avatar_url: 'https://images.mentalfloss.com/sites/default/files/styles/mf_image_16x9/public/blobfish_0.jpg?itok=mQZ9o70e&resize=1100x1100',
     // avatar_url: 'https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg',
   },
 ];
